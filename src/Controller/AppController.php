b28e2a9c9e67a879cd86ee4f8a1bbe19db3f33ea
KingTas1 <chertaslim15@gmail.com>
2025-10-25 17:52:00 +0200

    les img sont revenues!

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,31 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class AppController
 {
-    #[Route('/', name: 'app_spa_root', methods: ['GET'], priority: -100)]
+    #[Route('/', name: 'app_spa_root', methods: ['GET'])]
+    public function root(): RedirectResponse
+    {
+        return new RedirectResponse('/app');
+    }
 
-    #[Route(
-        '/{reactRouting',
-        requirements: ['reactRouting' => '^(?!api|mentions|app/|_(profiler|wdt)).*'],
-        methods: ['GET'],
-        priority: -100,
-    )]
-
-    #[Route(
-        '/{reactRouting}',
-        name: 'app_spa',
-        requirements: ['reactRouting' => '^(?!api|mentions|app/|_(profiler|wdt)).*'],
-        methods: ['GET']
-    )]
+    #[Route('/app', name: 'app_index', methods: ['GET'])]
+    #[Route('/app/{reactRouting}', name: 'app_spa', requirements: ['reactRouting' => '.*'], methods: ['GET'])]
     public function spa(KernelInterface $kernel): BinaryFileResponse
     {
         $file = $kernel->getProjectDir() . '/public/app/index.html';
-
         return new BinaryFileResponse($file);
     }
 }
-
